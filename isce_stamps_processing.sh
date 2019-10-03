c8e6f5ab60f7d4a46dbc6f94f04b29f9de4a957b
torresal <alexander.p.torres@jpl.nasa.gov>
2019-10-03 00:54:34 -0700

    Update isce_stamps_processing.sh

--- isce_stamps_processing.sh
+++ isce_stamps_processing.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
 # copy packages from isce_stamps_processing repo 
-sudo cp -rf $HOME/isce_stamps_processing/pkgs/ $HOME/verdi/pkgs/
+cp -rf $HOME/isce_stamps_processing/pkgs/* $HOME/verdi/pkgs/
 
 # source paths for StaMPS processing
-source $HOME/isce_stamps_processing/pkgs/StaMPS-master/StaMPS_CONFIG.bash 
+source $HOME/isce_stamps_processing/pkgs/StaMPS-master/StaMPS_CONFIG.bash
 
-if ![ -f "$TRIANGLE_BIN/triangle" ]; then
-	cd $HOME/verdi/pkgs/triangle/src
-	make
-	cp triangle bin/
+if [[ -e $TRIANGLE_BIN/triangle ]]
+then
+    echo Triangle Bin Exists!
+else
+    cd $HOME/verdi/pkgs/triangle
+    make
+    mkdir bin
+    cp triangle bin/
 fi
 
-if ![ -f "$SNAPHU_BIN/snaphu" ]; then
-	cd $HOME/verdi/pkgs/snaphu-v2.0.0/src
-	make
+if [[ -e $SNAPHU_BIN/snaphu ]]
+then
+    echo Snaphu Bin Exists!
+else
+    cd $HOME/verdi/pkgs/snaphu-v2.0.0/src
+    make
 fi
 
 # run StaMPS processing
